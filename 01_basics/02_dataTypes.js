ceb9e0f10ebbc98948e1b0e9b7454af8ebcc27ca
Vikash Gupta <35304465+vikashQA92@users.noreply.github.com>
2024-12-15 12:24:56 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+let age = 18
+let isLoggedIn = false
+let state;
+
+//alert( 3 + 3) // We are using nodejs, not browser
+
+// numberRange => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value & its a Type of Object
+// undefined => value is not yet assigned
+// symbol => help to find unique
+
+// object
+
+console.log()
