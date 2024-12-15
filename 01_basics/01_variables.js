35abe74a256ad98099f22f071256c348e6424f91
Vikash Gupta <35304465+vikashQA92@users.noreply.github.com>
2024-12-15 11:47:47 +0000

    Variables learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 34567
+let accountEmail =  "vikash@yopmail.com"
+var accountPassword = "123456"   
+accountcity= "Patna"
+let HigherEducation;
+/*
+Prefer not to use of VAR because of issue in block scope and functional scope
+
+*/
+
+accountEmail= "QATesting@yopmail.com"
+accountPassword = "9999999"
+accountcity = "Gaya"
+
+console.table([accountEmail, accountPassword, accountcity, HigherEducation])
